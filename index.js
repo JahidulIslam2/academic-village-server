32bf0b55f5a5148bc6edc36fc5cdf825e0d0f306
Jahidul Islam <jahid2111@gmail.com>
2022-10-26 11:45:19 +0600

    course category created'

--- index.js
+++ index.js
@@ -2,9 +2,18 @@ const express =require('express');
 const app =express();
 const port=process.env.PORT || 5000;
 
+const courses=require('./courseData/courseCategories.json')
 
 app.get('/',(req,res)=>{
     res.send('Server Run')
+})
+
+app.get('/courses-name',(req,res)=>{
+    res.send(courses);
+})
+
+app.get('/course/:id',(req,res)=>{
+    const id =req.params.id;
 })
 
 app.listen(port,()=>{
